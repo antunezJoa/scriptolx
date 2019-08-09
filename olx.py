5251d500100479af9ccad507fae423a267915e27
OH <laboratorio@OH-04>
2019-08-09 12:18:08 -0300

    Minimas correcciones

--- olx.py
+++ olx.py
@@ -6,17 +6,20 @@ import os.path
 domain = 'https://www.olx.com.ar/autos-cat-378'
 marcas = []
 y = 0
-j = 1
+index = 1
 
 response = requests.get(domain)
 
 soup = BeautifulSoup(response.text, "html.parser")
+
+#obtengo el link de la siguiente pagina
 
 for z in range(234,235):
         tag2 = soup.findAll('a')[z]
         next_page = tag2['href']
         next_page_link = 'https:' + next_page
 
+#obtengo las imagenes y sus descripciones
 
 for i in range(3,33):
         one_a_tag = soup.findAll('img')[i]
@@ -31,8 +34,8 @@ for i in range(3,33):
 
         if not os.path.exists(path):
             os.makedirs(path)
-            urllib.request.urlretrieve(link, "./download/olx/" + desc2[y].lower().replace(' ', '-') + "/" + str(desc2[y]).lower() + "-carID=" + str(i) + ".jpg")
-            print("Descargada foto",i-2, "de la pagina", j)
+            urllib.request.urlretrieve(link, './download/olx/' + desc2[y].lower().replace(' ', '-') + '/' + str(desc2[y]).lower() + '-carID=' + str(i-1) + '.jpg')
+            print("Descargada la foto",i-2, "de la pagina", index)
         else:
-            urllib.request.urlretrieve(link, "./download/olx/" + desc2[y].lower().replace(' ', '-') + "/" + str(desc2[y]).lower() + "-carID=" + str(i) + ".jpg")
-            print("Descargada foto",i-2, "de la pagina", j)
+            urllib.request.urlretrieve(link, './download/olx/' + desc2[y].lower().replace(' ', '-') + '/' + str(desc2[y]).lower() + '-carID=' + str(i-1) + '.jpg')
+            print("Descargada la foto",i-2, "de la pagina", index)
