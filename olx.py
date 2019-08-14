3dacbe39af206537c5952e5dfbefbb10f9ed79c8
OH <laboratorio@OH-04>
2019-08-14 10:20:57 -0300

    update

--- olx.py
+++ olx.py
@@ -3,10 +3,11 @@ import urllib.request
 from bs4 import BeautifulSoup
 import os.path
 
-domain = 'https://www.olx.com.ar/autos-cat-378'
+domain = 'https://www.olx.com.ar/autos-cat-378' #-p-1476
+flag = True
 marcas = []
 y = 0
-index = 1
+index = 1 #nro de pagina
 
 response = requests.get(domain)
 
@@ -16,26 +17,43 @@ soup = BeautifulSoup(response.text, "html.parser")
 
 for z in range(234,235):
         tag2 = soup.findAll('a')[z]
-        next_page = tag2['href']
-        next_page_link = 'https:' + next_page
+        tag22 = str(tag2)
+        if 'rel' in tag22:
+            rel = tag2['rel']
+            print("EXISTE REL")
+            if rel == ['next']:
+                print("ES NEXT")
+                next_page = tag2['href']
+                next_page_link = 'https:' + next_page
+            else:
+                print("NO ES NEXT")
+                print("LAST PAGE")
+                flag = False
+        else:
+            flag = False
+            print("NO EXISTE REL")
+            print("LAST PAGE")
 
 #obtengo las imagenes y sus descripciones
 
 for i in range(3,33):
-        one_a_tag = soup.findAll('img')[i]
-        link = one_a_tag['src']
-        desc = one_a_tag['alt']
+        tag = soup.findAll('img')[i]
+        link = tag['src']
+        desc = tag['alt']
         desc2 = desc.split(' ',33)
         marca = desc2[y]
         marcas += [marca]
         marcas = sorted(set(marcas)) #elimino duplicados y ordeno la lista de marcas
 
-        path = './download/olx/' + desc2[y].lower().replace(' ', '-') + '/'
+        #¿eliminar vacios?
+
+        path = './download/olx/' + desc2[y].lower().replace(' ', '-') + '/' + str(i-1) + "-" + str(index) + '/'
 
         if not os.path.exists(path):
             os.makedirs(path)
-            urllib.request.urlretrieve(link, './download/olx/' + desc2[y].lower().replace(' ', '-') + '/' + str(desc2[y]).lower() + '-carID=' + str(i-1) + '.jpg')
+            #urllib.request.urlretrieve(link, './download/olx/' + desc2[y].lower().replace(' ', '-') + '/' + str(desc2[y]).lower() + '-carID=' + str(i-1) + '.jpg')
+            urllib.request.urlretrieve(link, './download/olx/' + desc2[y].lower().replace(' ', '-') + '/' + str(i-1) + "-" + str(index) + '/' + str(desc2[y]).lower() + '.jpg')
             print("Descargada la foto",i-2, "de la pagina", index)
         else:
-            urllib.request.urlretrieve(link, './download/olx/' + desc2[y].lower().replace(' ', '-') + '/' + str(desc2[y]).lower() + '-carID=' + str(i-1) + '.jpg')
+            urllib.request.urlretrieve(link, './download/olx/' + desc2[y].lower().replace(' ', '-') + '/' + str(i-1) + "-" + str(index) + '/' + str(desc2[y]).lower() + '.jpg')
             print("Descargada la foto",i-2, "de la pagina", index)
