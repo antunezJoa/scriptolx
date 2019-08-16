d6cd42fa5a6bebf4e340acd109d484d47e933960
OH <laboratorio@OH-04>
2019-08-16 12:27:36 -0300

    fixes

--- olx.py
+++ olx.py
@@ -5,41 +5,26 @@ import os.path
 import re
 
 domain = 'https://www.olx.com.ar/autos-cat-378'
-links_paginas = []
-k = 2
-y = 0
-lista = []
-lista2 = []
-q = 0
-lista_ids = []
 
 response = requests.get(domain)
 soup = BeautifulSoup(response.text, "html.parser")
 
-#obtengo los 30 links de las 30 publaciones que hay por cada pagina
+'''for para obtener el numero de la ultima pagina'''
 
-for i in range(108,227):
-    tag = soup.findAll('a')[i]
-    href = tag['href']
-    href2 = 'https:' + href
-    lista2 += [href2]
-lista_nueva = []
-for i in lista2:
-    if i not in lista_nueva:
-        lista_nueva.append(i)
-lista_nueva.remove('https:')
+lista = []
 
-#for para obtener el numero de la ultima pagina
-
-for i in range (0,1):
+for i in range(0, 1):
     tag2 = soup.findAll('p')[i]
     tag2 = str(tag2)
-    tag2 = tag2.replace('.','')
+    tag2 = tag2.replace('.', '')
     lista = re.findall('\d+', tag2)
 
-#while para crear el array con los links de todas las páginas siguientes
+'''while para crear el array con los links de todas las páginas'''
 
-while k<=int(lista[1]):
+k = 2
+links_paginas = []
+
+while k <= int(lista[1]):
     links_paginas += ['https://www.olx.com.ar/autos-cat-378-p-' + str(k)]
     k = k + 1
 
@@ -47,92 +32,79 @@ links_paginas.reverse()
 links_paginas.append("https://www.olx.com.ar/autos-cat-378")
 links_paginas.reverse()
 
-#entrar a cada item y descargar todas las fotos,id
-##################################################
-
 j = 0
-while (j <= len(links_paginas)):
-    urls = links_paginas[j]
-    response = requests.get(urls)
+
+while j <= len(links_paginas):
+    url = links_paginas[j]
+    response = requests.get(url)
     soup = BeautifulSoup(response.text, "html.parser")
 
-    for x in range(71, 350):
-        tag3 = soup.findAll('a')[x]
+    '''obtengo lista de ID's'''
+
+    lista_ids = []
+
+    for i in range(71, 350):
+        tag3 = soup.findAll('a')[i]
         tag33 = str(tag3)
         if 'data-id' in tag33:
-            id = tag3['data-id']
-            lista_ids += [id]
+            ID = tag3['data-id']
+            lista_ids += [ID]
 
-    for i in range(3,33):
-            tag4 = soup.findAll('img')[i]
-            link = tag4['src']
-            desc = tag4['alt']
-            desc2 = desc.split(' ',33)
+    '''obtengo los links de las publaciones que hay por cada pagina'''
 
-            path = './download/olx/' + desc2[y].lower().replace(' ', '-') + '/' + str(lista_ids[q]) + '/'
+    lista2 = []
+
+    for i in range(108, 227):
+        tag = soup.findAll('a')[i]
+        href = tag['href']
+        href2 = 'https:' + href
+        lista2 += [href2]
+
+    links_per_page = []
+
+    for i in lista2:
+        if i not in links_per_page:
+            links_per_page.append(i)
+    links_per_page.remove('https:')
+
+    u = 0
+    while u <= len(links_per_page):
+        url2 = links_per_page[u]
+        response = requests.get(url2)
+        soup = BeautifulSoup(response.text, "html.parser")
+
+        '''obtengo la marca del auto de la publicacion en la que me encuentro'''
+
+        marca2 = []
+
+        for i in range(31, 32):
+            tag = soup.findAll('span')[i]
+            marca = tag.string
+            marca2 = marca.split(' ', 1)
+
+        '''obtengo los links de las imagenes de la publicacion en la que me encuentro'''
+
+        image = []
+        q = 0
+
+        for i in range(0, len(soup.findAll('img'))):
+            tag = soup.findAll('img')[i]
+            tagg = str(tag)
+            if 'data-modal-image' in tagg:
+                image = tag['data-modal-image']
+
+            path = './download/olx/' + str(marca2[0]).lower().replace(' ', '-') + '/' + str(lista_ids[q]) + '/'
+
+            #LINKS FULL DE LAS IMAGENES, JSON CON LOS METADATOS Y ARREGLAR DEMAS ERRORES
 
             if not os.path.exists(path):
                 os.makedirs(path)
-                urllib.request.urlretrieve(link, './download/olx/' + desc2[y].lower().replace(' ', '-') + '/' + str(lista_ids[q]) + '/' + str(desc2[y]).lower() + '.jpg')
+                urllib.request.urlretrieve(image, './download/olx/' + str(marca2[0]).lower().replace(' ', '-') + '/' + str(lista_ids[q]) + '/' + str(marca2[0]).lower() + '.jpg')
                 q = q + 1
-                print("Descargada imagen numero",i-2,"de la pagina",j+1)
+                print("Descargada la imagen", i + 1, "de la publicacion", u + 1, "de la pagina", j + 1)
             else:
-                urllib.request.urlretrieve(link, './download/olx/' + desc2[y].lower().replace(' ', '-') + '/' + str(lista_ids[q]) + '/' + str(desc2[y]).lower() + '.jpg')
+                urllib.request.urlretrieve(image, './download/olx/' + str(marca2[0]).lower().replace(' ', '-') + '/' + str(lista_ids[q]) + '/' + str(marca2[0]).lower() + '.jpg')
                 q = q + 1
-                print("Descargada imagen numero",i-2,"de la pagina",j+1)
-    j = j + 1
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-'''for z in range(234,235):
-        tag2 = soup.findAll('a')[z]
-        tag22 = str(tag2)
-        if 'rel' in tag22:
-            rel = tag2['rel']
-            print("EXISTE REL")
-            if rel == ['next']:
-                print("ES NEXT")
-                next_page = tag2['href']
-                next_page_link = 'https:' + next_page
-                paginas += [next_page_link]
-            else:
-                print("NO ES NEXT")
-                print("LAST PAGE")
-                flag = False
-        else:
-            flag = False
-            print("NO EXISTE REL")
-            print("LAST PAGE")'''
+                print("Descargada la imagen", i + 1, "de la publicacion", u + 1, "de la pagina", j + 1)
+    u = u + 1
+j = j + 1
--- olx2.py
+++ olx2.py
@@ -4,100 +4,13 @@ from bs4 import BeautifulSoup
 import os.path
 import re
 
-domain = 'https://www.olx.com.ar/autos-cat-378'
-q = 0
+domain = 'https://mardelplata.olx.com.ar/citroen-aircross-iid-1058334945'
 
 response = requests.get(domain)
 soup = BeautifulSoup(response.text, "html.parser")
 
-'''for para obtener el numero de la ultima pagina'''
-
-lista = []
-
-for i in range(0, 1):
-    tag2 = soup.findAll('p')[i]
-    tag2 = str(tag2)
-    tag2 = tag2.replace('.', '')
-    lista = re.findall('\d+', tag2)
-
-'''while para crear el array con los links de todas las páginas siguientes'''
-
-k = 2
-links_paginas = []
-
-while k <= int(lista[1]):
-    links_paginas += ['https://www.olx.com.ar/autos-cat-378-p-' + str(k)]
-    k = k + 1
-
-links_paginas.reverse()
-links_paginas.append("https://www.olx.com.ar/autos-cat-378")
-links_paginas.reverse()
-
-j = 0
-
-while j <= len(links_paginas):
-    url = links_paginas[j]
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, "html.parser")
-
-    lista_ids = []
-
-    for x in range(71, 350):
-        tag3 = soup.findAll('a')[x]
-        tag33 = str(tag3)
-        if 'data-id' in tag33:
-            ID = tag3['data-id']
-            lista_ids += [ID]
-
-    '''obtengo los 30 links de las 30 publaciones que hay por cada pagina'''
-
-    lista2 = []
-
-    for i in range(108, 227):
-        tag = soup.findAll('a')[i]
-        href = tag['href']
-        href2 = 'https:' + href
-        lista2 += [href2]
-
-    lista_nueva = []
-
-    for i in lista2:
-        if i not in lista_nueva:
-            lista_nueva.append(i)
-    lista_nueva.remove('https:')
-
-    u = 0
-    while u <= len(lista_nueva):
-        url2 = lista_nueva[u]
-        response = requests.get(url2)
-        soup = BeautifulSoup(response.text, "html.parser")
-
-        marca2 = []
-
-        for i in range(31, 32):
-            tag = soup.findAll('span')[i]
-            marca = tag.string
-            marca2 = marca.split(' ', 1)
-
-        image = []
-
-        for i in range(0, len(soup.findAll('img'))):
-            tag = soup.findAll('img')[i]
-            tagg = str(tag)
-            if 'data-modal-image' in tagg:
-                image = tag['data-modal-image']
-                print(image)
-
-            path = './download/olx/' + str(marca2[0]).lower().replace(' ', '-') + '/' + str(lista_ids[q]) + '/'
-
-            if not os.path.exists(path):
-                os.makedirs(path)
-                urllib.request.urlretrieve(image, './download/olx/' + str(marca2[0]).lower().replace(' ', '-') + '/' + str(lista_ids[q]) + '/' + str(marca2[0]).lower() + '.jpg')
-                q = q + 1
-                print("Descargada la imagen", i + 1, "de la publicacion", u + 1, "de la pagina", j + 1)
-            else:
-                urllib.request.urlretrieve(image, './download/olx/' + str(marca2[0]).lower().replace(' ', '-') + '/' + str(lista_ids[q]) + '/' + str(marca2[0]).lower() + '.jpg')
-                q = q + 1
-                print("Descargada la imagen", i + 1, "de la publicacion", u + 1, "de la pagina", j + 1)
-            u = u + 1
-    j = j + 1
+for i in range(0, len(soup.findAll('a'))):
+    tag = soup.findAll('a')[i]
+    href = tag['href']
+    if 'full' in str(href):
+        image = href
