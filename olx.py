715e4e8a3ebc619a0aa9b5828aa60bba3f1cfb41
OH <laboratorio@OH-04>
2019-09-20 08:49:44 -0300

    end

--- olx.py
+++ olx.py
@@ -11,7 +11,7 @@ headers = {'User-Agent':
                'Chrome/50.0.2661.102 Safari/537.36'}
 
 
-def downloadlinks():
+def savelinks():
     response = requests.get(domain, headers=headers)
     soup = BeautifulSoup(response.text, "html.parser")
 
@@ -76,101 +76,107 @@ def downloadlinks():
                 json.dump(links, file)
             print("Saved", links['url' + str(c)], "number of links saved:", c)
             c += 1
-    print("End")
+    print("Link saved")
+
+
+def downloaddata():
+    with open('./download/olx/item_links.json', 'r') as f:
+        domains = f.read()
+
+    doms = json.loads(domains)
+
+    count = 0  # count es el contador que indica en cual link arranca la descarga de imagenes
+
+    links_number = 10  # aca va el numero de links guardados en el json
+
+    while count <= links_number:
+        url_public = doms['url' + str(count)]
+        print(url_public, count)
+
+        response = requests.get(url_public, headers=headers)
+        soup = BeautifulSoup(response.text, "html.parser")
+
+        # obtengo el ID de la publicacion en la que me encuentro
+
+        url_public_str = str(url_public)
+        ides = re.findall('\d+', url_public_str)
+        ides = list(map(int, ides))
+        ID = max(ides)
+
+        # obtengo los datos del vehiculo, obtengo la marca y luego creo el json
+
+        data_vehicle = {}
+        fields = []
+
+        for li_tag in soup.findAll('ul', {'class': 'item_partials_optionals_view compact'}):
+            for span_tag in li_tag.find_all('li'):
+                value = span_tag.find('span').text
+                field = span_tag.find('strong').text
+                fields += [field]
+                data_vehicle[field] = value
+
+        if 'Marca / Modelo:' in fields:
+            brand = data_vehicle['Marca / Modelo:'].split(' ', 1)[0]
+
+        elif 'Marca:' in fields:
+            brand = data_vehicle['Marca:']
+        else:
+            brand = "unknown"
+
+        path = './download/olx/' + str(brand).lower().replace(' ', '-') + '/' + str(ID) + '/'
+
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+        # creo el archivo .json con las características del vehiculo
+
+        with open(path + 'meta.json', 'w') as fp:
+            json.dump(data_vehicle, fp)
+
+        print("Created meta.json")
+
+        # obtengo los links de las imagenes de la publicacion en la que me encuentro
+
+        q = 0
+        images2 = []
+        images = []
+
+        for i in range(0, len(soup.findAll('a'))):
+            tag = soup.findAll('a')[i]
+            href = tag['href']
+            if 'image;p=full' in href:
+                q = q + 1
+                images += [href]
+
+        if len(images) == 0:
+            for i in range(0, len(soup.findAll('img'))):
+                tag = soup.findAll('img')[i]
+                source = tag['src']
+                if 'image;p=full' in source:
+                    q = 1
+                    images2 += [source]
+
+        for i in images2:
+            if i not in images:
+                images.append(i)
+
+        y = 0
+
+        while y < q:
+            urllib.request.urlretrieve(images[y], './download/olx/' + str(brand).lower().replace(' ', '-') + '/' + str(
+                ID) + '/' + str(brand).lower() + '_' + str(ID) + '_' + str(y + 1) + '.jpg')
+            print("Downloaded image", y + 1, "/", brand)
+            y = y + 1
+
+        count = count + 1
+
+    print("Images downloaded")
 
 
 path = './download/olx/item_links.json'
 
 if not os.path.exists(path):
-    downloadlinks()
+    savelinks()
+    downloaddata()
 else:
-    with open('./download/olx/item_links.json', 'r') as f:
-        domains = f.read()
-
-doms = json.loads(domains)
-
-count = 0  # count es el contador que indica en cual link arranca la descarga de imagenes
-
-links_number = 100  # aca va el numero de links guardados en el json
-
-while count <= links_number:
-    url_public = doms['url' + str(count)]
-    print(url_public, count)
-
-    response = requests.get(url_public, headers=headers)
-    soup = BeautifulSoup(response.text, "html.parser")
-
-    # obtengo el ID de la publicacion en la que me encuentro
-
-    url_public_str = str(url_public)
-    ides = re.findall('\d+', url_public_str)
-    ides = list(map(int, ides))
-    ID = max(ides)
-
-    # obtengo los datos del vehiculo, obtengo la marca y luego creo el json
-
-    data_vehicle = {}
-    fields = []
-
-    for li_tag in soup.findAll('ul', {'class': 'item_partials_optionals_view compact'}):
-        for span_tag in li_tag.find_all('li'):
-            value = span_tag.find('span').text
-            field = span_tag.find('strong').text
-            fields += [field]
-            data_vehicle[field] = value
-
-    if 'Marca / Modelo:' in fields:
-        brand = data_vehicle['Marca / Modelo:'].split(' ', 1)[0]
-
-    elif 'Marca:' in fields:
-        brand = data_vehicle['Marca:']
-    else:
-        brand = "unknown"
-
-    path = './download/olx/' + str(brand).lower().replace(' ', '-') + '/' + str(ID) + '/'
-
-    if not os.path.exists(path):
-        os.makedirs(path)
-
-    # creo el archivo .json con las características del vehiculo
-
-    with open(path + 'meta.json', 'w') as fp:
-        json.dump(data_vehicle, fp)
-
-    print("Created meta.json")
-
-    # obtengo los links de las imagenes de la publicacion en la que me encuentro
-
-    q = 0
-    images2 = []
-    images = []
-
-    for i in range(0, len(soup.findAll('a'))):
-        tag = soup.findAll('a')[i]
-        href = tag['href']
-        if 'image;p=full' in href:
-            q = q + 1
-            images += [href]
-
-    if len(images) == 0:
-        for i in range(0, len(soup.findAll('img'))):
-            tag = soup.findAll('img')[i]
-            source = tag['src']
-            if 'image;p=full' in source:
-                q = 1
-                images2 += [source]
-
-    for i in images2:
-        if i not in images:
-            images.append(i)
-
-    y = 0
-
-    while y < q:
-        urllib.request.urlretrieve(images[y], './download/olx/' + str(brand).lower().replace(' ', '-') + '/' + str(ID) + '/' + str(brand).lower() + '_' + str(ID) + '_' + str(y + 1) + '.jpg')
-        print("Downloaded image", y + 1, "/", brand)
-        y = y + 1
-
-    count = count + 1
-
-print("End")
+    downloaddata()
